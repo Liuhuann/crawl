a03a92c7b0f8dd015984496598c2d6ede2b0dde5
Liuhuann <450570185@qq.com>
2015-05-14 15:58:34 +0800

    更新

--- scripts/fetch_latest.py
+++ scripts/fetch_latest.py
@@ -21,6 +21,7 @@ from histar.processor.mxwang.worker import MXWWork
 from histar.processor.tupianzj.worker import TupzjWork
 from histar.processor.ifeng.worker import IFengWork 
 from histar.processor.dazyl.worker import DZYLWork 
+from histar.processor.qoocc.worker import JXRDWork
 
 if __name__ == '__main__':
     worker_list = []
@@ -69,6 +70,7 @@ if __name__ == '__main__':
     worker_list.append( TupzjWork(total_page_count=5) )
     worker_list.append( IFengWork(total_page_count=5) )
     worker_list.append( DZYLWork(total_page_count=5) )
+    worker_list.append( JXRDWork(total_page_count=5) )
     for worker in worker_list:
         p = Process(target=worker)
         p.start()
